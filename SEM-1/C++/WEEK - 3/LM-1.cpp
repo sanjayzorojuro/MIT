869013908bcab644dd502defb368daf03cdac9c5
sanjayzorojuro <sanjayraops17@gmail.com>
2026-08-20 21:11:00 +0530

    c++ week 3 example and practice codes

---
+++ SEM-1/C++/WEEK - 3/LM-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+inline int cube(int n){
+	return n*n*n;
+}
+
+int main(){
+	
+	int n;
+	cout<<"Enter a number to chcek for cube:";	
+	cin>>n;
+	
+	cout<<"cube of the number is:"<<cube(n);
+	
+	
+	return 0;
+	
+}
+
